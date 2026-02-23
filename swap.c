9d460ca7bbd9bc40623b7a215bcb1c4567658bd4
krupal007147 <lohkrupal2007@gmail.com>
2026-02-23 19:23:00 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+  int a,b;
+  printf("ENTER VALUE OF a:");
+  scanf("%d",&a);
+  printf("ENTER VALUE OF b:");
+  scanf("%d",&b);
+
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("a=%d",a);
+  printf("b=%d",b);
+}
